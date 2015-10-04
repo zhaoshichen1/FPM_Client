3e34905c1063d2ab20f737f7880ef1897a700314
zhaoshichen <zhaoshichen1@gmail.com>
2015-10-04 19:05:20 +0800

    Alerts works well. Now we are working for Dashboard

---
+++ client/app/scripts/controllers/dashboard.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alertsApp.controller:DashboardCtrl
+ * @description
+ * # DashboardCtrl
+ * Controller of the alertsApp
+ */
+angular.module('clientApp')
+  .controller('DashboardCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/signup.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:SignUpCtrl
+ * @description
+ * # SignUpCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp').
+  controller('SignupCtrl', function ($scope, $http, $log, alertService, $location) {
+
+      $scope.whileSubmit = function() {
+
+        var payload = {
+            codename: $scope.codename,
+            org: $scope.org,
+            password: $scope.password
+        };
+
+        $http.post('app/signup', payload).error(function(data, status){
+                // bad request casued by "maybe" existing user with the same codename
+                if(status === 400){
+
+
+                    angular.forEach(data,
+                        function(value, key)
+                        {
+
+                            if(key === "codename" || key === "password" || key === "org" )
+                                alertService.add('danger', key + ' : ' + value);
+                            else
+                                alertService.add('danger', value.message);
+                        }
+                    )
+                }
+
+                if(status === 500){
+                    alertService.add('danger', 'Internal Server Error!!');
+                }
+              }).success(
+
+          function(data){
+
+            console.log('IN');
+            console.log(data);
+            debug = data;
+
+            if(data.hasOwnProperty('success')){
+              $location.path(
+                '/dashboard'
+              )
+            }
+          });
+
+
+
+      }
+  });
